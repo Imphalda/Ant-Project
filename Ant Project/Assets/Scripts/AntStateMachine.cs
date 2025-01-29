4af3b7f7e360c97bc9bd54d937be656ff03b698d
Imphalda <134883994+Imphalda@users.noreply.github.com>
2025-01-29 14:45:01 -0600

    Oh boy
    yep that nearly worked

--- Ant Project/Assets/Scripts/AntStateMachine.cs
+++ Ant Project/Assets/Scripts/AntStateMachine.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Android.Types;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Rendering;
@@ -15,7 +15,8 @@ public class AntStateMachine : MonoBehaviour
 {
     public AntState state = AntState.Searching;
     public GameObject food;
-
+    public GameObject home;
+    [Range(0.0f, 10.0f)] public float duration = 5.0f;
     private void Update()
     {
         switch (state)
@@ -40,15 +41,47 @@ public class AntStateMachine : MonoBehaviour
 
     private void Search()
     {
-        Collider[] cols = Physics.OverlapSphere()
+        Collider[] cols = Physics.OverlapSphere(transform.position, 1);
+        
+        foreach (Collider col in cols)
+        {
+            if (col.CompareTag("Food"))
+            {
+                state = AntState.Harvesting;
+                break;
+            }
+            StartCoroutine(MoveToward(duration));
+        }
     }
     private void Harvest()
     {
-
+        StartCoroutine(wait());
+        state = AntState.Returning;
     }
     private void ReturnHome()
     {
-        
+        transform.position = home.transform.position;
+        state = AntState.Searching;
+    }
+
+    private IEnumerator MoveToward(float duration)
+    {
+        Vector3 startPos = transform.position;
+        Vector3 endPos = new Vector3(Random.Range(-9.0f, 9.0f), transform.position.y, Random.Range(-9.0f, 9.0f));
+        float timer = 0;
+
+        while (timer < duration)
+        {
+            float percent = timer / duration;
+            transform.position = Vector3.Lerp(startPos, endPos, percent);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    private IEnumerator wait()
+    {
+        yield return new WaitForSeconds(3);
     }
 }
 
